6856bac188c22e47520b2ea8824fb8fdbd312fe2
Tlex-Ahomas <alex.thomas@snet.net>
2024-04-19 09:08:16 -0400

    Recipie Processor try-exception Add try-exception that catches if image link is not found

--- recipieProcessor.py
+++ recipieProcessor.py
@@ -1,15 +1,18 @@
 from recipie import Recipie
 import json
 
+
 class RecipieProcessor:
+    def __init__(self):
+        self.recipies = []
+
     def load_recipies(self, json_file):
         # opens json file
         js = open(json_file, encoding="utf-8")
         recipie_data = json.load(js, )
-        recipies = []
         # stores each recipie in json in the recipies list
         for i in recipie_data:
-            recipies.append(Recipie(*i.values()))
+            self.recipies.append(Recipie(*i.values()))
 
         # characters for loading bar
         bar = "█"
@@ -17,8 +20,19 @@ class RecipieProcessor:
         green = "\33[32m"
         reset = "\33[0m"
         # loops through recipes, downloads images with loading bar to keep track
-        for r in range(len(recipies)):
-            bar_count = int(((r+1)/len(recipies)) * 75)
-            print(f"Downloading image {r+1} of {len(recipies)} {green}{bar*bar_count}{rem_bar*(75-bar_count)}{reset}", end="\r")
-            recipies[r].set_image(recipies[r].image_url)
+        for r in range(len(self.recipies)):
+            bar_count = int(((r+1)/len(self.recipies)) * 75)
+            print(f"Downloading image {r+1} of {len(self.recipies)} {green}{bar*bar_count}{rem_bar*(75-bar_count)}{reset}", end="\r")
+            self.recipies[r].set_image(self.recipies[r].image_url)
         print("")
+
+    def recipies(self):
+        return self.recipies
+
+    def get_num_recipies(self):
+        return len(self.recipies)
+
+    def get_recipie(self, ind):
+        if 0 <= ind < len(self.recipies):
+            return self.recipies[ind]
+        raise Exception('index out of bounds')
